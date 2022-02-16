d96531e81f1b0c69302a49ef2f9783e758a7e5d5
Sahil Mansoori <sahil.mansoori.143@gmail.com>
2022-02-16 23:23:35 +0530

    LL: Swap Nodes in Pairs, DLC

---
+++ Leetcode/Problems/Medium/SwapNodesInPairs.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head and head.next:
+            temp = head.next
+            head.next = self.swapPairs(temp.next)
+            temp.next = head
+            return temp
+        return head
+            
+            
+        
