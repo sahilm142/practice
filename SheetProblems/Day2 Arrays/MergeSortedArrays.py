0674f056a62cc6fb3c75b2c831e68e69d6475daa
Sahil Mansoori <sahil.mansoori.143@gmail.com>
2022-02-22 17:16:05 +0530

    Day2: Merge Sorted Array

---
+++ SheetProblems/Day2 Arrays/MergeSortedArrays.py
@@ -0,0 +1,26 @@
+#BruteForce: Just second array and sort 
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        nums1[m:] = nums2
+        nums1.sort() 
+            
+
+#traverse and check if elements from both the arrays
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        while n:
+            if m and nums1[m-1]>= nums2[n-1]:
+                nums1[m+n-1] = nums1[m-1]
+                m-=1
+            else:
+                nums1[m+n-1] = nums2[n-1]
+                n-=1
+            
+            
+        
